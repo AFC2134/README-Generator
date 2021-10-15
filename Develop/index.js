f4765facff6f46a03d5080acc546eeb929bfd8a9
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-15 18:48:22 -0500

    init function first pass, may need to revise

--- Develop/index.js
+++ Develop/index.js
@@ -61,7 +61,13 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        writeToFile('README.md', markdown(answers))
+    })
+}
 
 // Function call to initialize app
 init();
