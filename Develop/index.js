aaefcd6c913eab912d1594320fae567e23de25e7
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-15 20:55:16 -0500

    package lock fixed

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+const markDown = require('../Develop/utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -58,7 +58,18 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+fs.writeFile(fileName, data, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("successfully rendered file");
+        console.log(data);
+    }
+
+})
+}
+
 
 // TODO: Create a function to initialize app
 function init() {
